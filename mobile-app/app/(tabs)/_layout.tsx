6342d03f1dde7387e22eff167f1974a42a623c4d
Rafael <rafasilva9537@gmail.com>
2025-04-14 11:38:58 -0300

    Add svg icons in tabs

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -1,39 +1,94 @@
 import { Tabs } from "expo-router";
+import { HomeSvg, ProfileSvg, StarSvg, PlusCircleSvg } from "@/constants/icons";
+import { StyleSheet, View, Text } from "react-native"
 import React from "react";
+import { colors } from "@/constants/colors";
+import { SvgProps } from "react-native-svg";
+
+interface TabIconProps {
+    focused: boolean, 
+    SvgIcon: React.FC<SvgProps> // used to show 'imported svg'
+}
+
+const TabText = ({children} : any) => {
+    return (
+        <Text style={{fontSize: 14}}>{children}</Text>
+    )
+}
+
+const TabIcon = ({ focused, SvgIcon }: TabIconProps) => {
+    if(focused){
+        return (
+            <View style={[styles.tabIcon, styles.focusedTabIcon]}>
+                <SvgIcon width={30} height={30}/>
+            </View>
+        )
+    }
+ 
+    return (
+        <View style={styles.tabIcon}>
+            <SvgIcon width={30} height={30}/>
+        </View>
+    )
+}
 
 const Layout = () => {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+            }} 
+        >
             <Tabs.Screen 
                 name="index"
                 options={{
                     title: "Homepage",
-                    headerShown: false
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => <TabIcon focused={focused} SvgIcon={HomeSvg} />
                 }}
             />
             <Tabs.Screen 
                 name="new-route"
                 options={{
                     title: "Nova Rota",
-                    headerShown: false
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => <TabIcon focused={focused} SvgIcon={PlusCircleSvg} />
                 }}
             />
             <Tabs.Screen 
                 name="favorites"
                 options={{
                     title: "Favoritos",
-                    headerShown: false
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => <TabIcon focused={focused} SvgIcon={StarSvg} />
                 }}
             />
             <Tabs.Screen 
                 name="profile"
                 options={{
                     title: "Perfil",
-                    headerShown: false
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => <TabIcon focused={focused} SvgIcon={ProfileSvg} />
                 }}
             />
         </Tabs>
     )
 }
+
+const styles = StyleSheet.create({
+    tabIcon: {
+        gap: 0,
+        alignItems: "center",
+        justifyContent: "center",
+        minWidth: 75,
+        minHeight: 40,
+        borderRadius: 50,
+        top: "20%",
+        padding: 1,
+    },
+    focusedTabIcon: {
+        backgroundColor: colors.accent,
+    },
+});
 
 export default Layout;
---
+++ mobile-app/constants/icons.ts
@@ -0,0 +1,11 @@
+import HomeSvg from "@/assets/icons/home.svg";
+import StarSvg from "@/assets/icons/star.svg";
+import ProfileSvg from "@/assets/icons/profile-circle.svg";
+import PlusCircleSvg from "@/assets/icons/plus-circle.svg";
+
+export {
+    HomeSvg,
+    StarSvg,
+    ProfileSvg,
+    PlusCircleSvg
+};
