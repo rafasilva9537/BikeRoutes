7008377939ebe8aa62d0e34c05a88c83c1cc09f8
Rafael <rafasilva9537@gmail.com>
2025-04-15 16:48:53 -0300

    Improve design of homepage flatlist

--- mobile-app/app/(tabs)/index.tsx
+++ mobile-app/app/(tabs)/index.tsx
@@ -3,16 +3,17 @@ import React from "react";
 import { FlatList, Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 import bikeRoutes from "@/mock_data/bike-routes"
 import { colors } from "@/constants/colors";
+import { TimerSvg, DistanceSvg, StarSvg } from "@/constants/icons";
 
 interface BikeRoute {
   id: number,
   title: string,
   image: string,
   description: string,
-  duration: number,
-  distance: number,
-  rating: number,
-  average_speed: number
+  duration: number, // minutes
+  distance: number, // km
+  rating: number, // 0 to 5
+  average_speed: number // km/h
 }
 
 const RouteBox = (bikeRoute: BikeRoute) =>{ 
@@ -20,7 +21,25 @@ const RouteBox = (bikeRoute: BikeRoute) =>{
     <Link style={styles.routeBoxContainer} href={`/bike-routes/${bikeRoute.id}`} asChild>
       <TouchableOpacity>
         <Image source={ {uri: bikeRoute.image} } style={styles.routeImage}/>
-        <Text style={styles.routeTitle}>{bikeRoute.title}</Text>
+        <View style={styles.routeInfoContainer}>
+          <Text style={styles.routeTitle}>{bikeRoute.title}</Text>
+          <View style={styles.routeBoxInfoDetails} >
+            <View style={styles.routeIconInfoContainer}>
+              <DistanceSvg/>
+              <Text>{bikeRoute.distance} km</Text>
+            </View>
+
+            <View style={styles.routeIconInfoContainer}>
+              <TimerSvg/>
+              <Text>{Math.floor(bikeRoute.duration/60)}:{(bikeRoute.duration%60).toString().padEnd(2,"0")} hrs</Text>
+            </View>
+
+            <View style={styles.routeIconInfoContainer}>
+              <StarSvg fill={"yellow"}/>
+              <Text>{bikeRoute.rating}</Text>
+            </View>
+          </View>
+        </View>
       </TouchableOpacity>
     </Link>
   );
@@ -35,9 +54,7 @@ const Index = () => {
         data= {bikeRoutes}
         renderItem={({item}) => <RouteBox {... item}/>}
         keyExtractor={(item) => item.id.toString()}
-        numColumns={2}
         contentContainerStyle={styles.routeList}
-        columnWrapperStyle={styles.routeRowContainer}
       />
     </View>
   );
@@ -59,23 +76,39 @@ export const styles = StyleSheet.create({
   },
   routeList: {
     width: "100%",
-  },
-  routeRowContainer: {
-    width: "100%",
-    gap: 10,
-    justifyContent: "center",
-    paddingBottom: 15
+    gap: 15
   },
   routeBoxContainer: {
-    width: "45%",
+    width: "90%",
+    alignItems: "center",
+    alignSelf: "center"
   },
   routeTitle: {
     fontSize: 16,
+    fontWeight: "bold"
   },
   routeImage: {
     width: "100%",
-    height: 100,
-    borderRadius: 5
+    height: 150,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5
+  },
+  routeInfoContainer: {
+    backgroundColor: colors.secondary,
+    width: "100%",
+    alignItems: "center",
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+    padding: 5
+  },
+  routeBoxInfoDetails: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  routeIconInfoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
   }
 });
 
