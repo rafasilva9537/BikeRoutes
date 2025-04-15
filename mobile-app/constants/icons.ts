3f8c45e1a62957d3e8fb8dd44b8026970ac13b05
Rafael <rafasilva9537@gmail.com>
2025-04-15 16:49:12 -0300

    Improve design of homepage flatlist

--- mobile-app/constants/icons.ts
+++ mobile-app/constants/icons.ts
@@ -2,10 +2,14 @@ import HomeSvg from "@/assets/icons/home.svg";
 import StarSvg from "@/assets/icons/star.svg";
 import ProfileSvg from "@/assets/icons/profile-circle.svg";
 import PlusCircleSvg from "@/assets/icons/plus-circle.svg";
+import DistanceSvg from "@/assets/icons/diameter.svg";
+import TimerSvg from "@/assets/icons/timer.svg";
 
 export {
     HomeSvg,
     StarSvg,
     ProfileSvg,
-    PlusCircleSvg
+    PlusCircleSvg,
+    DistanceSvg,
+    TimerSvg
 };
