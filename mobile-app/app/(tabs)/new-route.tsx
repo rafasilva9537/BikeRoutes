ba6ec84e6bf01218cae95c1876737a2274c3c347
MayColdHeart <devmayy@gmail.com>
2025-04-15 11:33:48 -0300

    Create a map estructure

--- mobile-app/app/(tabs)/new-route.tsx
+++ mobile-app/app/(tabs)/new-route.tsx
@@ -1,11 +1,55 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import MapView from 'react-native-maps';
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject } from 'expo-location'
 
-const NewRoute = () => {
-    return (
-        <View>
-            <Text>Nova rota aqui</Text>
+const newRoute = () => {
+const[location, setLocation] = useState <LocationObject | null> (null);
+  
+  async function requestLocationPermissions() {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if(granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+
+      console.log("Localizacao atual =>", currentPosition);
+    }
+    
+  }
+  
+
+useEffect(() => {
+  requestLocationPermissions();
+},[]);
+
+  return (
+        <View style={styles.container}>
+
+          <MapView style={styles.map} />
+
+
+
+
+            
         </View>
     )
 }
 
-export default NewRoute;
+export default newRoute;
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+
+  },
+
+  map:{
+    flex: 1,
+    width: '100%',
+
+  } 
+
+});
+
