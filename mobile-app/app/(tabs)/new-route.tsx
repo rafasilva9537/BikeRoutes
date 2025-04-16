c4c19f63ac69f10e1e1f120c6fbba69d0c3b0fa7
MayColdHeart <devmayy@gmail.com>
2025-04-16 08:50:32 -0300

    Sidebar Layout

--- mobile-app/app/(tabs)/new-route.tsx
+++ mobile-app/app/(tabs)/new-route.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheet, FlatList, TouchableOpacity, Dimensions } from "react-native";
 import MapView, { Marker, MapPressEvent, Region } from "react-native-maps";
 import { requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject } from "expo-location";
+import { colors } from "@/constants/colors";
 import {bikeRoutes} from "mock_data/bike-routes"
 
 // Contador de IDs de cada rota
@@ -77,14 +78,12 @@ const newRoute = () => {
         onPress={handleMapPress}
       >
         {markers.map((marker) => (
-          <Marker
-            key={marker.id}
-            coordinate={{
+          <Marker key={marker.id} coordinate={{
               latitude: marker.latitude,
               longitude: marker.longitude,
             }}
-            title={`ID: ${marker.id}`}
-            description="Marcador de rota"/>
+
+            title={`ID: ${marker.id}`}/>
             ))}
         </MapView>
       )}
@@ -94,6 +93,7 @@ const newRoute = () => {
 
       <View style={styles.sidebar}>
         <Text style={styles.sidebarTitle}> Rotas</Text>
+ 
         
         <FlatList
         data={markers}
@@ -120,28 +120,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row",
+    backgroundColor: colors.background,
   },
   map: {
     flex: 1,
-    width: width * 0.75, // 75% da tela para o mapa
+    width: '75%',
   },
   sidebar: {
-    width: width * 0.25, // 25% da tela para a barra lateral
-    backgroundColor: "#f0f0f0",
+    width: '25%',
+    backgroundColor: colors.background,
     padding: 10,
     borderLeftWidth: 1,
-    borderLeftColor: "#ccc",
+    borderLeftColor: colors.primary,
   },
   sidebarTitle: {
     marginBottom: 10,
-    fontSize: 16,
+    fontSize: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.primary
   },
   routeItem: {
     paddingVertical: 8,
     borderBottomWidth: 1,
-    borderBottomColor: "#40fbac",
+    borderBottomColor: colors.primary,
   },
   routeText: {
-    fontSize: 15,
+    fontSize: 16,
+    textAlign: "center",
+    borderColor: colors.primary,
+    backgroundColor: colors.secondary,
+    borderRadius: 5,
+    borderWidth: 1,
   },
 });
