6a79a1adcf06ec228f9e0f1affd45f793373b3d5
rafasilva9537 <rafasilva9537@gmail.com>
2025-06-17 22:16:03 -0300

    Add `GeographyUtils` with Haversine distance calculation method

---
+++ api/BikeRoutesApi/Utils/GeographyUtils.cs
@@ -0,0 +1,23 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace BikeRoutesApi.Utils;
+
+public static class GeographyUtils
+{
+    public static double CalculateHaversineDistance(Point point, Point point2)
+    {
+        const double R = 6371 * 1000; // Mean earth radius in meters
+        var dLat = ToRadians(point2.X - point.X);
+        var dLon = ToRadians(point2.Y - point.Y);
+        point.X = ToRadians(point.X);
+        point2.X = ToRadians(point2.X);
+   
+        var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Sin(dLon / 2) * Math.Sin(dLon / 2) * Math.Cos(point.X) * Math.Cos(point2.X);
+        var c = 2 * Math.Asin(Math.Sqrt(a));
+        return R * 2 * Math.Asin(Math.Sqrt(a));
+    }
+    
+    private static double ToRadians(double angle) {
+        return Math.PI * angle / 180.0;
+    }
+}
